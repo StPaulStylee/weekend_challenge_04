b6bbf83b2fa8b227357becda4e37593f400683b4
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2016-10-16 18:26:37 -0500

    All routes besides PUT are in. Also, appending function needs fix on delete request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,24 +1,63 @@
-$(function(){
+$(function () {
   getTasks();
+  $('#addTask').on('submit', addTask);
+  $('#tasks').on('click', '.delete', deleteTask);
 }); // End of document Ready
 
-function getTasks () {
+function getTasks() {
   $.ajax({
     type: 'GET',
-    url:'/tasks',
-    success: displayTasks
+    url: '/tasks',
+    success: displayTasks,
   });
 }
 
 function displayTasks(response) {
   console.log('working.... Yes!');
   var $taskList = $('#tasks');
-  $taskList.empty();
+
   //If needed, add a counter variable here so you can create unique id's for each task
-  response.forEach(function(task) {
-    var $div = $('<div></div>');
-    $div.append('<p>' + task.to_do + '</p>');
-    $div.append('<p>' + task.complete + '</p>');
+  response.forEach(function (task) {
+    var $div = $('<ul class ="task"></ul>');
+
+    //$div.append('<p>' + task.complete + '</p>');
+    $div.append('<li><input type="checkbox" name="complete" value="true" /></li>');
+    $div.append('<li>' + task.to_do + '</li>');
+    var $deleteLi = $('<li></li>');
+    var $deleteButton = $('<button type="button" class="delete">Delete</button>');
+    $deleteButton.data('id', task.id);
+    $deleteLi.append($deleteButton);
+    $div.append($deleteLi);
     $('#tasks').append($div);
+  });
+}
+
+function addTask(event) {
+  event.preventDefault();
+  var taskData = {};
+  var taskBoolean = { name: 'complete', value: 'false' };
+  var fields = $(this).serializeArray();
+  fields.push(taskBoolean);
+  fields.forEach(function (element) {
+    taskData[element.name] =  element.value;
+  });
+
+  console.log(taskData);
+
+  $.ajax({
+    type: 'POST',
+    url: '/tasks',
+    data: taskData,
+    success: displayTasks,
+  });
+}
+
+function deleteTask(event) {
+  event.preventDefault();
+  var $taskID = $(this).data('id');
+  $.ajax({
+    type: 'DELETE',
+    url: '/tasks/' + $taskID,
+    success: displayTasks,// This is incorrect. Not displaying correctly.
   });
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,24 +2,26 @@ var router = require('express').Router();
 var pg = require('pg');
 
 var config = {
-  database: 'rho'
+  database: 'rho',
 };
 
 var pool = new pg.Pool(config);
 router.get('/', function (req, res) {
-  pool.connect(function(err, client, done) {
+  pool.connect(function (err, client, done) {
     try {
       if (err) {
         console.log('Error connecting to DB', err);
         res.sendStatus(500);
         return;
       }
+
       client.query('SELECT * FROM tasks ORDER BY id;', function (err, result) {
         if (err) {
-          console.log ('Error querying the DB', err);
+          console.log('Error querying the DB', err);
           res.sendStatus(500);
           return;
         }
+
         res.send(result.rows);
       });
     } finally {
@@ -27,4 +29,63 @@ router.get('/', function (req, res) {
     }
   });//end of pool.connect
 }); //end of get request
+
+router.post('/', function (req, res) {
+  pool.connect(function (err, client, done) {
+    try {
+      if (err) {
+        console.log('Issue connecting the DB', err);
+        res.sendStatus(500);
+        return;
+      }
+
+      client.query('INSERT INTO tasks (to_do, complete) VALUES ($1, $2) returning *',
+                    [req.body.to_do, req.body.complete],
+                function (err, result) {
+                  if (err) {
+                    console.log('Issue querying the DB', err);
+                    res.sendStatus(500);
+                    return;
+                  }
+
+                  res.send(result.rows);
+                });
+    }// end of try
+    finally {
+      done();
+    }
+  });//End of pool.connect
+});//End of post request
+
+router.delete('/:id', function (req, res) {
+//When did these params become available? LIke... how is the router accessing this?
+  var id = req.params.id;
+  console.log(id);
+  pool.connect(function (err, client, done) {
+    try {
+      if (err) {
+        console.log('Error connecting to the DB', err);
+        res.sendStatus(500);
+        return;
+      }
+
+      client.query('DELETE FROM tasks WHERE id=$1', [id],
+        function (err) {
+          if (err) {
+            console.log('Error querying the DB', err);
+            res.sendStatus(500);
+            return;
+          }
+
+          res.sendStatus(204);
+        });
+    }
+    finally {
+      done();
+    }
+  });//End of pool.connect
+});//End of delete request
+
+
+
 module.exports = router;
